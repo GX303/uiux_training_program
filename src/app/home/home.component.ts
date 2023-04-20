231ef9f9726d447f2ec72d944dbb0331b1078d85
Guanjie Xu <Guanjie_Xu@jabil.com>
2023-04-20 18:47:10 +0800

    implement store optimistic data modification

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareRe
 import { HttpClient } from '@angular/common/http';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
-import { CoursesService } from '../services/courses.service';
+import { CoursesStore } from '../services/courses.store';
 import { LoadingService } from '../loading/loading.service';
 import { MessagesService } from '../messages/messages.service';
 
@@ -22,7 +22,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private coursesService: CoursesService, private loadingService: LoadingService, private messagesService: MessagesService) {
+  constructor(private coursesStore: CoursesStore) {
 
   }
 
@@ -31,19 +31,9 @@ export class HomeComponent implements OnInit {
   }
 
   reloadCourses() {
-    const courses$ = this.coursesService.loadAllCourses().pipe(
-      map(courses => courses.sort(sortCoursesBySeqNo)),
-      catchError(err => {
-        const message = "Could not load courses";
-        this.messagesService.showErrors(message);
-        console.log(message, err);
-        return throwError(err);
-      })
-    );
-    const loadCourses$ = this.loadingService.showLoaderUntilCompleted<Course[]>(courses$);
-
-    this.beginnerCourses$ = loadCourses$.pipe(map(courses => courses.filter(course => course.category == "BEGINNER")));
-    this.advancedCourses$ = loadCourses$.pipe(map(courses => courses.filter(course => course.category == "ADVANCED")));
+    
+    this.beginnerCourses$ = this.coursesStore.filterByCategory("BEGINNER");
+    this.advancedCourses$ = this.coursesStore.filterByCategory("ADVANCED");
   }
 
 }
